bc64b9b8630a22134df636abc3c9a49092694841
Nakashima Masaya <nakashima-masaya@dmm.com>
2022-03-27 16:30:18 +0900

    27. Remove Element

---
+++ problem/00027/00027.go
@@ -0,0 +1,21 @@
+package _0027
+
+import "sort"
+
+// 27. Remove Element
+// https://leetcode.com/problems/remove-element/
+
+func RemoveElement(nums []int, val int) int {
+	count := 0
+	for i, v := range nums {
+		if v == val {
+			nums[i] = 100
+		} else {
+			count++
+		}
+	}
+
+	sort.Ints(nums)
+	nums = nums[:count]
+	return count
+}
