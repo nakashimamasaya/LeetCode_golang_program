d99d997c036e7ead2fc1c06b3f0376c765bddcb6
Nakashima Masaya <49140002+nakashimamasaya@users.noreply.github.com>
2022-05-05 00:15:04 +0900

    main.go delete 00704

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	_0206 "LeetCode/problem/00206"
-	_0704 "LeetCode/problem/00704"
 	"fmt"
 )
 
@@ -10,10 +9,6 @@ func main() {
 	// 206. Reverse Linked List
 	separate("206")
 	fmt.Println(_0206.ReverseList(_0206.SetSample()).Val)
-
-	// 704. Binary Search
-	separate("704")
-	fmt.Println(_0704.Search([]int{-1, 0, 3, 5, 9, 12}, 9))
 }
 
 func separate(num string) {
