6dd73fc87df2aae732741765217d0b891736e839
Nakashima Masaya <nakanaka1826@gmail.com>
2022-06-19 23:10:22 +0900

    000181
    wrong gorm
    latest gorm use

--- db/seeds/seed.go
+++ db/seeds/seed.go
@@ -4,16 +4,17 @@ import (
 	"fmt"
 	"log"
 
+	_00181 "LeetCode/db/seeds/problem/00181"
 	_00182 "LeetCode/db/seeds/problem/00182"
 	_00183 "LeetCode/db/seeds/problem/00183"
 	models "LeetCode/models"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func openConnection() *gorm.DB {
-	db, err := gorm.Open("mysql", "root:password@tcp(mysql:3306)/TEST?charset=utf8mb4&parseTime=True&loc=Local&timeout=1m")
+	db, err := gorm.Open(mysql.Open("root:password@tcp(mysql:3306)/TEST?charset=utf8mb4&parseTime=True&loc=Local&timeout=1m"), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Couldn't establish database connection: %s", err)
 	}
@@ -22,6 +23,7 @@ func openConnection() *gorm.DB {
 
 func Seed(db *gorm.DB) error {
 	message := []error{
+		_00181.Seed(db),
 		_00182.Seed(db),
 		_00183.Seed(db),
 	}
@@ -33,9 +35,14 @@ func Seed(db *gorm.DB) error {
 
 func main() {
 	db := openConnection()
-	defer db.Close()
 
-	db.AutoMigrate(models.Customers{}, models.Orders{}, models.Person{})
+	db.AutoMigrate(
+		models.Customers{},
+		models.Employee{},
+		models.Orders{},
+		models.Person{},
+	)
+
 	if err := Seed(db); err != nil {
 		fmt.Printf("%+v", err)
 		return
