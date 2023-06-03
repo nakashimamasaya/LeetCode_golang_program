ae6b0d081ba1b4c7accc5b08918e091067432165
Nakashima Masaya <49140002+nakashimamasaya@users.noreply.github.com>
2023-06-03 13:23:00 +0900

    02529

---
+++ problem/02529/02529.go
@@ -0,0 +1,22 @@
+package _02529
+
+import "math"
+
+// 2529. Maximum Count of Positive Integer and Negative Integer
+// https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/
+
+func maximumCount(nums []int) int {
+	ans := 0
+	for i, n := range nums {
+		if n > 0 {
+			ans = int(math.Max(float64(ans), float64(len(nums)-i)))
+			break
+		}
+		if n == 0 {
+			continue
+		}
+		ans++
+	}
+
+	return ans
+}
